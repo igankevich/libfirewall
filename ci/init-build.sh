e1919443253ce69034f748ba07eafabcc2783097
Ivan Gankevich <igankevich@capybaramail.xyz>
2023-08-27 08:26:17 +0200

    CI

--- ci/init-build.sh
+++ ci/init-build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+# shellcheck disable=SC1091
 set -ex
 apt-get update -qq
 apt-get -qq install --no-install-recommends curl ca-certificates
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh
-sh /tmp/rustup.sh --help
-sh /tmp/rustup.sh -y
-rustup default 1.72-x86_64-unknown-linux-gnu
+target=x86_64-unknown-linux-gnu
+toolchain=1.72-$target
+sh /tmp/rustup.sh -y --default-toolchain $toolchain --target $target
+. "$HOME/.cargo/env"
+rustup default $toolchain
