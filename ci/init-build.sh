73a576e4e86cb5ab36bcfba0a3a3aecbb6a12c13
Ivan Gankevich <igankevich@capybaramail.xyz>
2023-08-27 08:14:47 +0200

    CI

--- ci/init-build.sh
+++ ci/init-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -ex
+apt-get update -qq
+apt-get -qq install --no-install-recommends curl
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o /tmp/rustup.sh
 sh /tmp/rustup.sh
 rustup default 1.72-x86_64-unknown-linux-gnu
