3c5d85dfafa856a7674b232721b1ab75191bab90
Ivan Gankevich <igankevich@capybaramail.xyz>
2023-08-27 08:30:42 +0200

    CI

--- ci/binaries.sh
+++ ci/binaries.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+# shellcheck disable=SC1091
 set -ex
+. "$HOME/.cargo/env"
 rust_flags="-Ccodegen-units=1 -Cstrip=symbols -Copt-level=3 -Cincremental=false -Clto=yes -Cembed-bitcode=yes"
 target=x86_64-unknown-linux-gnu
 env RUSTFLAGS="$rust_flags" \
